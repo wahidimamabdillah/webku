0dc3f181007a137416a18390231792447a05956d
wahidimamabdillah <wahidimamabdillah62@gmail.com>
2024-05-02 21:48:37 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function login() {
+    alert("Login Berhasil");
+    window.location.href = "home.html";
+}
+
+function editprofil() {
+    alert("Profil Berhasil Di Edit");
+    window.location.href = "akun.html";
+}
